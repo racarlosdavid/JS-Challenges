ca147a1890bf7b106ae27fcdcad6fc55068f383b
racarlosdavid <2111724640101@ingenieria.usac.edu.gt>
2021-11-23 00:05:11 -0600

    fix 16 digits bug

--- 01/Facil.js
+++ 01/Facil.js
@@ -1,34 +1,35 @@
-
 function sumarUno(entrada){
     let salida = [];
-    let auxiliar = [];
     let multiplicador = 1;
     let result = 0;
+    let res = "";
+    let digitos = 1;
     //Paso el array a numero
     for (let index = entrada.length-1; index >= 0; index--) {
         const element = entrada[index];
-        result = result + element * multiplicador;
-        multiplicador = multiplicador * 10;   
+        if (digitos<=16) {
+            result = digitos == 1 ? result + ((element+1) * multiplicador) : result + (element * multiplicador); 
+            multiplicador = multiplicador * 10; 
+            digitos++;  
+        }else if(digitos == 17){
+            //Le sumo 1
+            res = result + "";
+            res = element + res ;
+            digitos++;  
+        }else{
+            res = element + res ;
+        }
+        
     }
-
-    //Le sumo 1
-    result = result+1;
-
-    //Separo el numero en digitos y lo paso a un array
-    while(result > 0){
-        salida.push(result%10);
-        result = Math.trunc(result/10);
+    
+    for (let index = 0; index < res.length; index++) {
+        const element = res[index];
+        salida.push(element) 
     }
-
-    //Invierto el orden 
-    for (let index = salida.length-1; index >= 0; index--) {
-        const element = salida[index];
-        auxiliar.push(element)  
-    }
-    return auxiliar;
+    return salida;
 }
 
-const res = sumarUno([1,2,3])
+const res = sumarUno([6,1,4,5,3,9,0,1,9,5,1,8,6,7,0,5,5,4,9,9,9,9,9,9])
 console.log(res)
 
 
